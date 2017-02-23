3c1f2aef39ccb10057bf4b1f6e215d5a5b077f4e
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-02-23 09:45:54 +0300

    [Test] Windows platform

--- SashaTest.py
+++ SashaTest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author: SashaChernykh
 # @Date: 2017-02-23 07:57:46
-# @Last Modified time: 2017-02-23 09:34:00
+# @Last Modified time: 2017-02-23 09:45:41
 
 print("Sasha Great Goddess!")
