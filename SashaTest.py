ba43d98bbd52806aa6989a23a8a39b8110513262
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-02-23 09:35:02 +0300

    [Test] flake8 error

--- SashaTest.py
+++ SashaTest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author: SashaChernykh
 # @Date: 2017-02-23 07:57:46
-# @Last Modified time: 2017-02-23 09:32:29
+# @Last Modified time: 2017-02-23 09:34:00
 
 print("Sasha Great Goddess!")
