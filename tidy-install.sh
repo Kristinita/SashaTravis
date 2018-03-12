7caa579477ce0569e65c789491fe4ae2fa266686
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-12 19:03:23 +0300

    ci(travis): Debugging $HOME variable
    $HOME variable instead of cd ../../../
    
    Fix #4

---
+++ tidy-install.sh
@@ -0,0 +1,19 @@
+# @Author: Kristinita
+# @Date:   2018-03-07 15:20:23
+# @Last Modified by:   Kristinita
+# @Last Modified time: 2018-03-12 19:01:08
+# Install HTML tidy on Ubuntu:
+# https://codeyarns.com/2016/06/06/how-to-build-and-install-html-tidy/
+echo Print script cwd
+pwd
+git clone https://github.com/htacg/tidy-html5.git
+cd tidy-html5
+cd build/cmake
+cmake ../..
+cmake --build . --config Release
+sudo make install
+echo Print final installation directory
+pwd
+cd $HOME
+echo Print directory after HOME
+pwd
