08b261b2564744d7079c3de49d52b2f271e1a161
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-12 19:14:38 +0300

    ci(travis): Try TRAVIS_BUILD_DIR instead of HOME

--- tidy-install.sh
+++ tidy-install.sh
@@ -1,7 +1,7 @@
 # @Author: Kristinita
 # @Date:   2018-03-07 15:20:23
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-12 19:01:08
+# @Last Modified time: 2018-03-12 19:12:52
 # Install HTML tidy on Ubuntu:
 # https://codeyarns.com/2016/06/06/how-to-build-and-install-html-tidy/
 echo Print script cwd
@@ -14,6 +14,6 @@ cmake --build . --config Release
 sudo make install
 echo Print final installation directory
 pwd
-cd $HOME
-echo Print directory after HOME
+cd $TRAVIS_BUILD_DIR
+echo Print directory after TRAVIS_BUILD_DIR
 pwd
