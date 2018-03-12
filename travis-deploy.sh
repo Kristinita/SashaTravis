db78fbc23fd5b135a2eeed9cc5c4c078d04dad2e
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-12 09:49:56 +0300

    ci(travis): Add GITHUB_OUTPUT_FOLDER variable

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,6 +3,7 @@ GH_USERNAME=Kristinita
 
 #advanced settings
 BRANCH=master
+GITHUB_OUTPUT_FOLDER=built_website
 PELICAN_OUTPUT_FOLDER=SashaDest
 TARGET_REPO=$GH_USERNAME/SashaTravis.github.io.git
 
@@ -13,9 +14,9 @@ git config --global user.name "Kristinita"
 commitHash=$(git rev-parse HEAD)
 commitMessage=$(git log -1 --pretty=%B)
 #get the current output in a separate directory
-git clone --quiet --branch=$BRANCH https://${GITHUB_API_KEY}@github.com/$TARGET_REPO $PELICAN_OUTPUT_FOLDER > /dev/null
+git clone --quiet --branch=$BRANCH https://${GITHUB_API_KEY}@github.com/$TARGET_REPO $GITHUB_OUTPUT_FOLDER > /dev/null
 #copy the new output
-cd $PELICAN_OUTPUT_FOLDER
+cd $GITHUB_OUTPUT_FOLDER
 rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* . --delete
 #add, commit and push files
 git add -A .
