ab5220cde56cca62c9f3be2b733c60446332c014
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-12 09:55:22 +0300

    ci(travis): Disable dev/null

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,14 +7,14 @@ GITHUB_OUTPUT_FOLDER=built_website
 PELICAN_OUTPUT_FOLDER=SashaDest
 TARGET_REPO=$GH_USERNAME/SashaTravis.github.io.git
 
-echo -e "Starting to deploy to GitHub Pages\n"
+echo -e "Starting to deploy to GitHub Pages"
 git config --global user.email "SashaChernykhEmpressOfUniverse@kristinita.ru"
 git config --global user.name "Kristinita"
 # Pull hash and commit message of the most recent commit
 commitHash=$(git rev-parse HEAD)
 commitMessage=$(git log -1 --pretty=%B)
 #get the current output in a separate directory
-git clone --quiet --branch=$BRANCH https://${GITHUB_API_KEY}@github.com/$TARGET_REPO $GITHUB_OUTPUT_FOLDER > /dev/null
+git clone --quiet --branch=$BRANCH https://${GITHUB_API_KEY}@github.com/$TARGET_REPO $GITHUB_OUTPUT_FOLDER
 #copy the new output
 cd $GITHUB_OUTPUT_FOLDER
 rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* . --delete
@@ -22,6 +22,6 @@ rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* . --delete
 git add -A .
 detailedMessage="Commit $commitHash pushed to GitHub Pages by Travis build $TRAVIS_BUILD_NUMBER"
 git commit -m "$commitMessage" -m "$detailedMessage"
-git push -fq origin $BRANCH > /dev/null || exit $?
-echo -e "Deploy completed\n"
+git push -fq origin $BRANCH
+echo -e "Deploy completed"
 
