6c25b8628eaad98986545c54d53b54e7218d39be
Kristinita <vspobog@gmail.com>
2019-10-17 10:09:06 +0300

    ci(travis): try remove additional shell scripts

--- KiraDeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-commitHash=$(git rev-parse HEAD)
-commitMessage=$(git log -1 --pretty=%B)
-detailedMessage="Commit https://github.com/$TRAVIS_REPO_SLUG/commit/$commitHash pushed to GitHub Pages by Travis CI build $TRAVIS_BUILD_NUMBER — https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-git commit -m "$commitMessage" -m "$detailedMessage"
