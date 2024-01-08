124f83230fdc90ec7d4998fd66c44e104827c295
Kristinita <17247677+Kristinita@users.noreply.github.com>
2024-01-08 21:30:46 +0300

    fix(typo): name of the file with list of packages

--- travis-install.sh
+++ travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 parallel ::: 'gem install travis' \
-	'sudo apt update && sudo apt-get install $(egrep -v "^(#|$)" snappackageslist) && sudo ln -s $(which fdfind) /usr/local/bin/fd'
+	'sudo apt update && sudo apt-get install $(egrep -v "^(#|$)" aptpackages) && sudo ln -s $(which fdfind) /usr/local/bin/fd'
