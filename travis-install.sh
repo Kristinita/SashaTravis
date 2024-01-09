ec9f89f54ca03396d527a4b4fe3ab5feead80ef4
Kristinita <17247677+Kristinita@users.noreply.github.com>
2024-01-09 22:02:35 +0300

    ci(travis): try egrep

--- travis-install.sh
+++ travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 parallel ::: "gem install travis" \
-	"sudo apt update && sudo apt install fd-find && sudo ln --symbolic $(which fdfind) /usr/local/bin/fd"
+	"sudo apt update && sudo apt install $(egrep --invert-match '^(#|$)' aptpackages) && sudo ln --symbolic $(which fdfind) /usr/local/bin/fd"
