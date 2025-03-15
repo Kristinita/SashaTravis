0da3c57525cbb7489240728767f21b92a480888c
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 18:22:29 +0300

    ci(ci): try to place fd and tidy binaries to Pipenv virtual environment use gh-release-install
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- bash/github-releases-install.sh
+++ bash/github-releases-install.sh
@@ -2,13 +2,11 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 17:58:35
+# @Last Modified time: 2025-03-15 18:17:58
 
-# Kira Goddess!
+
 pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/bin --verbose &
 
-# Kira Goddess!
 pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/bin/fd --verbose &
 
-# Kira Goddess!
 wait
--- batch/github-releases-install.bat
+++ batch/github-releases-install.bat
@@ -1,10 +1,9 @@
 @REM @Author: SashaChernykh
 @REM @Date: 2025-03-15 15:20:41
 @REM @Last Modified by: SashaChernykh
-@REM @Last Modified time: 2025-03-15 17:58:01
+@REM @Last Modified time: 2025-03-15 18:17:47
 
-@REM Kira Goddess!
+
 START /B CMD /C "pipenv run gh-release-install Kristinita/tidy-html5 tidy.exe .venv/Scripts --verbose"
 
-@REM Kira Goddess!
 START /B CMD /C "pipenv run gh-release-install sharkdp/fd fd-{tag}-x86_64-pc-windows-gnu.zip --extract fd-{tag}-x86_64-pc-windows-gnu/fd.exe .venv/Scripts/fd.exe --verbose"
