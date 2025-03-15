2d5cbaf44ccca61826ca09ae15ea0a9b9bf671ab
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 17:21:46 +0300

    ci(travis): remove checking unexisting path .venv/Scripts
    https://app.travis-ci.com/github/Kristinita/SashaTravis/builds/274563737#L245
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- github-releases-install.sh
+++ github-releases-install.sh
@@ -2,7 +2,7 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 17:17:31
+# @Last Modified time: 2025-03-15 17:20:20
 pipenv run gh-release-install Kristinita/tidy-html5 tidy "$TRAVIS_BUILD_DIR/.venv/bin" --verbose &
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" "$TRAVIS_BUILD_DIR/.venv/bin" --verbose &
+pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" "$TRAVIS_BUILD_DIR/.venv/bin/fd" --verbose &
 wait
