bd73de23e4903ea0bad96bd4b1bb15ae02647dc4
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 16:40:23 +0300

    ci(ci): try cwd environment variables
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- github-releases-install.sh
+++ github-releases-install.sh
@@ -2,7 +2,7 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 15:17:24
-pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/Scripts --verbose &
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/Scripts/fd --verbose &
+# @Last Modified time: 2025-03-15 16:39:11
+pipenv run gh-release-install Kristinita/tidy-html5 tidy "$TRAVIS_BUILD_DIR/.venv/Scripts" --verbose &
+pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" "$TRAVIS_BUILD_DIR/.venv/Scripts/fd" --verbose &
 wait
