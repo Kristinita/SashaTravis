2e15ecd53eedeca73167fa7c24400e83055807ce
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 17:18:07 +0300

    ci(travis): try to move binaries to the folder bin/, not Scripts/ on Linux
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- github-releases-install.sh
+++ github-releases-install.sh
@@ -2,7 +2,7 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 16:52:45
-pipenv run gh-release-install Kristinita/tidy-html5 tidy "$TRAVIS_BUILD_DIR/.venv/Scripts" --verbose &
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" "$TRAVIS_BUILD_DIR/.venv/Scripts" --verbose &
+# @Last Modified time: 2025-03-15 17:17:31
+pipenv run gh-release-install Kristinita/tidy-html5 tidy "$TRAVIS_BUILD_DIR/.venv/bin" --verbose &
+pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" "$TRAVIS_BUILD_DIR/.venv/bin" --verbose &
 wait
