95a8f95c8ca075077ea8a1a316ef9f968e8a876d
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 18:00:13 +0300

    ci(ci): try comments between lines of scripts
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- bash/github-releases-install.sh
+++ bash/github-releases-install.sh
@@ -2,7 +2,13 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 17:39:59
+# @Last Modified time: 2025-03-15 17:58:35
+
+# Kira Goddess!
 pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/bin --verbose &
+
+# Kira Goddess!
 pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/bin/fd --verbose &
+
+# Kira Goddess!
 wait
--- batch/github-releases-install.bat
+++ batch/github-releases-install.bat
@@ -1,7 +1,10 @@
 @REM @Author: SashaChernykh
 @REM @Date: 2025-03-15 15:20:41
 @REM @Last Modified by: SashaChernykh
-@REM @Last Modified time: 2025-03-15 16:39:42
-START /B CMD /C "pipenv run gh-release-install Kristinita/tidy-html5 tidy.exe %APPVEYOR_BUILD_FOLDER%/.venv/Scripts --verbose"
+@REM @Last Modified time: 2025-03-15 17:58:01
 
-START /B CMD /C "pipenv run gh-release-install sharkdp/fd fd-{tag}-x86_64-pc-windows-gnu.zip --extract fd-{tag}-x86_64-pc-windows-gnu/fd.exe %APPVEYOR_BUILD_FOLDER%/.venv/Scripts/fd.exe --verbose"
+@REM Kira Goddess!
+START /B CMD /C "pipenv run gh-release-install Kristinita/tidy-html5 tidy.exe .venv/Scripts --verbose"
+
+@REM Kira Goddess!
+START /B CMD /C "pipenv run gh-release-install sharkdp/fd fd-{tag}-x86_64-pc-windows-gnu.zip --extract fd-{tag}-x86_64-pc-windows-gnu/fd.exe .venv/Scripts/fd.exe --verbose"
