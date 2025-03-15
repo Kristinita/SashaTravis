cf655d5d640560a8eb7348192ae4a8c4997ecab8
Kristinita <Kristinita@users.noreply.github.com>
2025-03-15 17:49:44 +0300

    ci(ci): normalize paths as for the real project
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- bash/gh-release-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @Author: SashaChernykh
-# @Date: 2025-03-15 15:13:47
-# @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 15:17:24
-pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/Scripts --verbose &
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/Scripts/fd --verbose &
-wait
---
+++ bash/github-releases-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @Author: SashaChernykh
+# @Date: 2025-03-15 15:13:47
+# @Last Modified by: SashaChernykh
+# @Last Modified time: 2025-03-15 17:39:59
+pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/bin --verbose &
+pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/bin/fd --verbose &
+wait
