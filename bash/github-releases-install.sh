84ba339938177db5fd9789a19ee21c27abdcab7f
Kristinita <Kristinita@users.noreply.github.com>
2025-03-16 20:26:06 +0300

    ci(travis): try to check exit code without Pipenv and fd
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- bash/github-releases-install.sh
+++ bash/github-releases-install.sh
@@ -2,11 +2,9 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-15 18:17:58
+# @Last Modified time: 2025-03-16 20:20:15
 
 
-pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/bin --verbose &
-
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/bin/fd --verbose &
+pipenv run gh-release-install Kristinita/tidy-html5 tidy /usr/local/bin --verbose &
 
 wait
