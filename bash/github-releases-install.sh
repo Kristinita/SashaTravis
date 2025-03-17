9973063c49099ceef61592c11719ffb6b6d20d00
Kristinita <Kristinita@users.noreply.github.com>
2025-03-17 17:20:06 +0300

    ci(ci): try to run incorrect gh-release-install command from scripts
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- bash/github-releases-install.sh
+++ bash/github-releases-install.sh
@@ -2,11 +2,8 @@
 # @Author: SashaChernykh
 # @Date: 2025-03-15 15:13:47
 # @Last Modified by: SashaChernykh
-# @Last Modified time: 2025-03-16 20:34:41
+# @Last Modified time: 2025-03-17 17:17:01
 
-
-pipenv run gh-release-install Kristinita/tidy-html5 tidy .venv/bin --verbose &
-
-pipenv run gh-release-install sharkdp/fd "fd-{tag}-x86_64-unknown-linux-gnu.tar.gz" --extract "fd-{tag}-x86_64-unknown-linux-gnu/fd" .venv/bin/fd --verbose &
+gh-release-install Kristinita/tidy-html5 tidy.exe KiraNonexistentDirectory/KiraNonexistentSubdirectory --verbose &
 
 wait
--- batch/github-releases-install.bat
+++ batch/github-releases-install.bat
@@ -1,9 +1,7 @@
 @REM @Author: SashaChernykh
 @REM @Date: 2025-03-15 15:20:41
 @REM @Last Modified by: SashaChernykh
-@REM @Last Modified time: 2025-03-17 13:55:19
+@REM @Last Modified time: 2025-03-17 17:16:37
 
 
-START /B CMD /C pipenv run gh-release-install Kristinita/tidy-html5 tidy.exe .venv/Scripts --verbose
-
-START /B CMD /C pipenv run gh-release-install sharkdp/fd fd-{tag}-x86_64-pc-windows-gnu.zip --extract fd-{tag}-x86_64-pc-windows-gnu/fd.exe .venv/Scripts/fd.exe --verbose
+START /B CMD /C gh-release-install Kristinita/tidy-html5 tidy.exe KiraNonexistentDirectory/KiraNonexistentSubdirectory --verbose
