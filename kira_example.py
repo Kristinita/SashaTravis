411757ce3006f50218ca5cd7086be5f88701efd9
Kristinita <Kristinita@users.noreply.github.com>
2025-03-21 21:13:33 +0300

    ci(ci): try to format docstrings with brackets and URLs
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

---
+++ kira_example.py
@@ -0,0 +1,20 @@
+"""Docstring for the file.
+
+[INFO] First link:
+https://github.com/PyCQA/docformatter
+
+[INFO] Second link:
+https://pypi.org/project/docformatter/
+"""
+
+
+def kira_function():
+    """Docstring for the function.
+
+    Third link:
+    https://docformatter.readthedocs.io
+
+    Fourth link:
+    https://github.com/PyCQA/docformatter/releases
+    """
+    print("Kira Goddess!")
